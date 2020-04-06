7f4f25e8f9a3ec0f6e6f14f33ef0ee730f011ed9
melsand <melsand02@gmail.com>
2020-04-06 19:32:37 +0200

    Kontaktformulär, samt anpassning för responsivitet med media queries

--- JS/index.js
+++ JS/index.js
@@ -1,150 +1,51 @@
+$('body').addClass('stop-scrolling');
 if ('scrollRestoration' in history) {
   history.scrollRestoration = 'manual';
 }
 window.scrollTo(0,0);
 
-$('body').addClass('stop-scrolling');
+
+
 
 $(window).on("load",function(){
+  // Eftersom sidan tar typ en millisekund att ladda så blir loadingscreenen onödig
   // $(".loader-wrapper").fadeOut("slow"); 
-  
 });
 
 setTimeout(() => {  $(".loader-wrapper").fadeOut("slow"), $('body').removeClass('stop-scrolling'); }, 2000);
-
-
+// $('body').removeClass('stop-scrolling');
 
 
 
 AOS.init({
   // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  disable: false,
+  startEvent: 'DOMContentLoaded', 
+  initClassName: 'aos-init',
+  animatedClassName: 'aos-animate', 
+  useClassNames: false, 
+  disableMutationObserver: false, 
+  debounceDelay: 50, 
+  throttleDelay: 99, 
   
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 200, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  offset: 200,
+  delay: 0, 
+  duration: 700,
+  easing: 'ease',
+  once: false, 
+  mirror: false, 
+  anchorPlacement: 'top-bottom',
+  
 
 });
 
-// var image = document.getElementsByClassName('container1');
-// new simpleParallax(image, {
-// 	scale: 1.5
-// });
 
-// (function () {
-//   var follower, init, mouseX, mouseY, positionElement, printout, timer;
-
-//   follower = document.getElementById('follower');
-
-//   printout = document.getElementById('printout');
-
-//   mouseX = event => {
-//     return event.clientX;
-//   };
-
-//   mouseY = event => {
-//     return event.clientY;
-//   };
-
-//   positionElement = event => {
-//     var mouse;
-//     mouse = {
-//       x: mouseX(event),
-//       y: mouseY(event) };
-
-//     follower.style.top = mouse.y + 'px';
-//     return follower.style.left = mouse.x + 'px';
-//   };
-
-//   timer = false;
-
-//   window.onmousemove = init = event => {
-//     var _event;
-//     _event = event;
-//     return timer = setTimeout(() => {
-//       return positionElement(_event);
-//     }, 1);
-//   };
-
-// }).call(this);
-var html = document.documentElement;
-var body = document.body;
-
-var scroller = {
-  target: document.querySelector("#scroll-container"),
-  ease: 0.05, // <= scroll speed
-  endY: 0,
-  y: 0,
-  resizeRequest: 1,
-  scrollRequest: 0,
-};
-
-var requestId = null;
-
-TweenLite.set(scroller.target, {
-  rotation: 0.01,
-  force3D: true
+$( '.js-input' ).keyup(function() {
+  if( $(this).val() ) {
+     $(this).addClass('not-empty');
+  } else {
+     $(this).removeClass('not-empty');
+  }
 });
-
-window.addEventListener("load", onLoad);
-
-function onLoad() {    
-  updateScroller();  
-  window.focus();
-  window.addEventListener("resize", onResize);
-  document.addEventListener("scroll", onScroll); 
-}
-
-function updateScroller() {
-  
-  var resized = scroller.resizeRequest > 0;
-    
-  if (resized) {    
-    var height = scroller.target.clientHeight;
-    body.style.height = height + "px";
-    scroller.resizeRequest = 0;
-  }
-      
-  var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
-
-  scroller.endY = scrollY;
-  scroller.y += (scrollY - scroller.y) * scroller.ease;
-
-  if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-    scroller.y = scrollY;
-    scroller.scrollRequest = 0;
-  }
-  
-  TweenLite.set(scroller.target, { 
-    y: -scroller.y 
-  });
-  
-  requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-}
-
-function onScroll() {
-  scroller.scrollRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
-}
-
-function onResize() {
-  scroller.resizeRequest++;
-  if (!requestId) {
-    requestId = requestAnimationFrame(updateScroller);
-  }
-}
